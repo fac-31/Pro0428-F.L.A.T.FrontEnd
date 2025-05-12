13d29d7c20524231f4ac95c10ba63237f5c3ed8f
Anna van Wingerden <91270891+annavanwingerden@users.noreply.github.com>
2025-05-12 17:20:19 +0100

    Fridge formatting (#10)
    * Fridge formatting
    
    * prettier formatting

---
+++ src/theme.ts
@@ -0,0 +1,12 @@
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  typography: {
+    fontFamily: '"Jost", "Helvetica", "Arial", sans-serif',
+    h4: {
+      fontFamily: '"Jost", "Helvetica", "Arial", sans-serif',
+    },
+  },
+});
+
+export default theme;
