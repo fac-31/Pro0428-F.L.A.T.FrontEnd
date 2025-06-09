97ed8893dc0495cfc35d10a8617bd7b810ec3dec
chrislush <133811141+chrislush@users.noreply.github.com>
2025-06-09 16:21:05 +0100

    Removed commented out section from Fridge.tsx.

--- src/pages/Dashboard/Fridge.tsx
+++ src/pages/Dashboard/Fridge.tsx
@@ -56,35 +56,6 @@ const Fridge = () => {
       />
     </div>
   );
-
-  // return (
-  //       <Paper
-  //         elevation={4}
-  //         sx={{
-  //           width: 500,
-  //           height: 800,
-  //           borderRadius: '80px 80px 0 0',
-  //           position: 'relative',
-  //           display: 'flex',
-  //           flexDirection: 'column',
-  //           alignItems: 'center',
-  //           backgroundColor: '#d7ebf5',
-  //           perspective: '800px',
-  //           transformStyle: 'preserve-3d',
-  //         }}
-  //       >
-  //         <FridgeTopDoor onSectionClick={handleSectionClick} />
-  //         <FridgeBigDoor
-  //           isOpen={isOpen}
-  //           onToggle={() => setIsOpen(!isOpen)}
-  //           activeSection={activeSection}
-  //           data={houseInfo}
-  //           testDbData={testDbData}
-  //           loading={loading}
-  //         />
-  //       </Paper>
-  //     </Box>
-  //   </Container>
 };
 
 export default Fridge;
